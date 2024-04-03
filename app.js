071b797095bed4bfc1835842274adf6bb3c1d303
Varun-arivazhagan <164339817+Varun-arivazhagan@users.noreply.github.com>
2024-04-03 11:57:53 +0530

    from-login

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const collection = require("./Mongo.js");
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  // Add your logic here
+});
+
+app.post("/", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const check = await collection.findOne({ email: email });
+
+    if (check) {
+      res.json("exist");
+    } else {
+      res.json("notexist");
+    }
+  } catch (e) {
+    res.json("fail");
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  const data = {
+    email: email,
+    password: password,
+  };
+
+  try {
+    const check = await collection.findOne({ email: email });
+
+    if (check) {
+      res.json("exist");
+    } else {
+      res.json("notexist");
+      await collection.insertMany([data]);
+    }
+  } catch (e) {
+    res.json("fail");
+  }
+});
+
+app.listen(8000, () => {
+  console.log("port connected");
+});
---
+++ src/components/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react"
+import axios from "axios"
+import { useNavigate, Link } from "react-router-dom"
+import "./Login.css"
+
+function Login() {
+
+    const history=useNavigate();
+
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    async function submit(e){
+        e.preventDefault();
+
+        try{
+
+            await axios.post("http://localhost:8000/",{
+                email,password
+            })
+            .then(res=>{
+                if(res.data=="exist"){
+                    history("/home",{state:{id:email}})
+                }
+                else if(res.data=="notexist"){
+                    alert("User have not sign up")
+                }
+            })
+            .catch(e=>{
+                alert("wrong details")
+                console.log(e);
+            })
+
+        }
+        catch(e){
+            console.log(e);
+
+        }
+
+    }
+
+
+    return (
+        <div className="form-container">
+
+            <h2>Login</h2>
+
+            <form action="POST">
+                <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
+                <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password"  />
+                <button type="submit" onClick={submit}>Submit</button> 
+            </form>
+
+            <br />
+            <p>OR</p>
+            <br />
+
+            <Link to="/signup" className="link">Signup Page</Link>
+
+        </div>
+    )
+}
+
+export default Login
